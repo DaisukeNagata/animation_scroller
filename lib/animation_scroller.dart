feeb1a51513df1e45b8a388ca0b6bd06bd067305
大祐永田 <dbank0208@gmail.com>
2022-03-31 22:25:08 +0900

    add: Version 0.1.3 changes to null safety

--- lib/animation_scroller.dart
+++ lib/animation_scroller.dart
@@ -6,75 +6,102 @@ class AnimationScroller extends ScrollController{
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 
-  int? duration = 0;
-  bool? animationFlg = true;
-  double? scrollOffset = 0.0;
-  double? keyboardHeight = 0.0;
-  double? _offsetDy = 0.0;
+  bool?   animationFlg    = true;
+  int?    duration        = 0;
+  double? scrollOffset    = 0.0;
+  double? keyboardHeight  = 0.0;
+  double? _offsetDy       = 0.0;
   double? _animationValue = 0.0;
   double? _containerValue = 0.0;
 
   scrollState(ScrollNotification scrollNotification) {
+
     if (scrollNotification is ScrollStartNotification) {
 
     } else if (scrollNotification is ScrollUpdateNotification) {
 
     } else if (scrollNotification is ScrollEndNotification) {
       scrollOffset = position.maxScrollExtent;
-      if (scrollOffset != 0 && scrollOffset > _containerValue && animationFlg) {
-        animationLogic(duration);
+      bool aFlg = (animationFlg ?? false);
+      int dValue = (duration ?? 0);
+      double aValue = (_animationValue ?? 0.0);
+      double cValue = (_containerValue ?? 0.0);
+      double offsetValue = (scrollOffset ?? 0.0);
+
+      if (offsetValue != 0 && offsetValue > cValue && aFlg) {
+        animationLogic(dValue);
       }
-      if (position.maxScrollExtent - offset == _containerValue) {
-        animationFlg = false;
+      if (position.maxScrollExtent - offsetValue == cValue) {
+        aFlg = false;
       }
     }
   }
 
   reset() {
+
     scrollOffset = 0;
     jumpTo(0.0);
     animationFlg = false;
   }
 
   scrollReturn(int value) {
-    animateTo(_animationValue,
+    double aValue = (_animationValue ?? 0.0);
+
+    animateTo(aValue,
         duration: Duration(milliseconds: value),
         curve: Curves.linear);
   }
 
   widgetBuild(BuildContext context, double containerValue, int duration) {
+    bool aFlg = (animationFlg  ?? false);
+    double kValue = (keyboardHeight ?? 0.0);
+
     _containerValue = containerValue;
-    if (MediaQuery.of(context).viewInsets.bottom != 0 && animationFlg) {
-      keyboardHeight = keyboardHeight <= MediaQuery.of(context).viewInsets.bottom ?
+    if (MediaQuery.of(context).viewInsets.bottom != 0 && aFlg) {
+      kValue = kValue <= MediaQuery.of(context).viewInsets.bottom ?
       MediaQuery.of(context).viewInsets.bottom :
-      keyboardHeight;
+      kValue;
     }
 
-    if (MediaQuery.of(context).viewInsets.bottom != 0 && hasClients && animationFlg) {
-      scrollOffset = scrollOffset <= position.maxScrollExtent ?
-      position.maxScrollExtent : scrollOffset;
+    if (MediaQuery.of(context).viewInsets.bottom != 0 && hasClients && aFlg) {
+      scrollOffset = (scrollOffset ?? 0.0) <= position.maxScrollExtent ?
+      position.maxScrollExtent : (scrollOffset ?? 0.0);
 
-      if (scrollOffset != 0 && scrollOffset > _containerValue && animationFlg) {
+      bool aFlg = (animationFlg  ?? false);
+      double cValue = (_containerValue ?? 0.0);
+      double offsetValue = (scrollOffset ?? 0.0);
+      if (offsetValue != 0 && offsetValue > cValue && aFlg) {
         animationLogic(duration);
       }
     }
   }
 
   focusLogic(FocusNode focusNode, double value, RenderBox box, double offsetFlg, int duration) {
+
     switch (focusNode.hasFocus) {
       case true:
         _offsetDy = box.localToGlobal(Offset.zero).dy;
         animationFlg = true;
-        _offsetDy > offsetFlg ? Future(() {animationLogic(duration);}) : null;
+
+        double offsetDyValue = (_offsetDy ?? 0.0);
+        double offsetValue = (scrollOffset ?? 0.0);
+
+        offsetDyValue > offsetFlg ? Future(() {animationLogic(duration);}) : null;
         break;
     }
   }
 
   animationLogic(int duration) {
     Future(() {
-      if (scrollOffset != 0 && scrollOffset > _containerValue && animationFlg) {
-        _animationValue = scrollOffset - _containerValue;
-        animateTo(_animationValue, duration: Duration(milliseconds: duration), curve: Curves.linear);
+      bool aFlg = (animationFlg ?? false);
+      double offsetValue = (scrollOffset ?? 0.0);
+      double cValue = (_containerValue ?? 0.0);
+
+      if (offsetValue != 0 && offsetValue > cValue && aFlg) {
+        _animationValue = offsetValue - cValue;
+        double aValue = (_animationValue ?? 0.0);
+
+        animateTo(aValue, duration: Duration(milliseconds: duration), curve: Curves.linear);
       }
     });
   }
